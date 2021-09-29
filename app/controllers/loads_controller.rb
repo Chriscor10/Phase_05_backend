27fd9e0f41cf067c9dbb20234e08aada99f628bb
christopher <coronac511@gmail.com>
2021-09-29 14:53:24 -0400

    added controllers

--- app/controllers/loads_controller.rb
+++ app/controllers/loads_controller.rb
@@ -1,2 +1,43 @@
 class LoadsController < ApplicationController
+    def index 
+        loads = Load.all
+        render json: loads
+    end
+
+    def show
+        loads = Load.find_by(loads_params[:id])
+        render json: loads
+    end
+
+    def create
+        loads = Load.create!(loads_params)
+        render json: loads, status: created
+    end
+
+    def update
+        loads = Load.find_by(loads_params[:id])
+        loads.update(loads_params)
+        render json: loads
+    end
+
+    def destroy
+        loads = Load.find_by(loads_params[:id])
+        loads.destroy
+        head :no_content
+    end
+        
+    private
+
+    def loads_params
+        params.permit( :container_size,
+            :load_weight,
+            :origin,
+            :destination,
+             :price,
+             :pickup_time,
+             :dropoff_time,
+            :pickup_date,
+             :dropoff_date,
+            :shipper,)
+    end
 end
--- app/controllers/shippers_controller.rb
+++ app/controllers/shippers_controller.rb
@@ -1,2 +1,34 @@
 class ShippersController < ApplicationController
+    def index 
+        shipper = Shipper.all
+        render json: shipper
+    end
+
+    def show
+        shipper = Shipper.find_by(shipper_params[:id])
+        render json: shipper
+    end
+
+    def create
+        shipper = Shipper.create!(shipper_params)
+        render json: shipper, status: created
+    end
+
+    def update
+        shipper = Shipper.find_by(shipper_params[:id])
+        shipper.update(shipper_params)
+        render json: shipper
+    end
+
+    def destroy
+        shipper = Shipper.find_by(shipper_params[:id])
+        shipper.destroy
+        head :no_content
+    end
+        
+    private
+
+    def shipper_params
+        params.permit(:company_name, :email)
+    end
 end
